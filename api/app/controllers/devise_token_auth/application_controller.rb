2f81ed4586d189b88c2bf152c655e4b77b119765
Michał Siwek <mike21@aol.pl>
2018-10-25 17:51:44 +0200

    Reopen the devise_token_auth/application_controller instead of redefining it

--- api/app/controllers/devise_token_auth/application_controller.rb
+++ api/app/controllers/devise_token_auth/application_controller.rb
@@ -1,5 +1,5 @@
-module DeviseTokenAuth
-  class ApplicationController < DeviseController
-    skip_forgery_protection
-  end
+require_dependency DeviseTokenAuth::Engine.config.root.join('app', 'controllers', 'devise_token_auth', 'application_controller.rb').to_s
+
+class DeviseTokenAuth::ApplicationController
+  skip_forgery_protection
 end
