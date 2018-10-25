e08d2da3bba8b692468ca52562a9d8d0292cc5fb
Michał Siwek <mike21@aol.pl>
2018-10-25 07:49:50 +0200

    Ensure legacy columns exist before migrating them

--- api/db/migrate/20120530054546_rename_api_key_to_spree_api_key.rb
+++ api/db/migrate/20120530054546_rename_api_key_to_spree_api_key.rb
@@ -3,7 +3,9 @@
 class RenameApiKeyToSpreeApiKey < ActiveRecord::Migration[4.2]
   def change
     unless defined?(User)
-      rename_column :spree_users, :api_key, :spree_api_key
+      if column_exists?(:spree_users, :api_key)
+        rename_column :spree_users, :api_key, :spree_api_key
+      end
     end
   end
 end
